50ec8d3117ea8f2c9736a727ff7fb2731636790e
Fco. Javier Guerrero Carmona <fjguerreroc@icloud.com>
2017-12-17 10:32:47 +0100

    HouseCells added. FlatList in grid

---
+++ src/sections/houses/HousesCell.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { TouchableOpacity, Image, StyleSheet, Dimensions, Platform } from 'react-native'
+
+export default class HousesCell extends Component { 
+    
+    // Propiedades por defecto, sirven para evitar fallos y documentar el comp.
+    static defaultProps = {
+        onSelect    : () => {},
+        item        : {}
+    }
+
+    render() {
+        //console.log("this.props", this.props)
+        const item = this.props.item
+        const onSelect = this.props.onSelect
+
+        const image = item.image_dir ? { uri: item.image_dir } : null
+        
+        return (
+            <TouchableOpacity style={styles.container} onPress = { () => onSelect(item) }>
+                <Image source={image} style={styles.imageStyle} resizeMode={'contain'}/>
+            </TouchableOpacity>
+        )
+    } 
+}
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 10,
+        width: Dimensions.get('window').width / 2 - 20, //857x600px,
+        height: (Dimensions.get('window').width /2 -20) * (857/600),
+
+        ...Platform.select({
+            ios: {
+                shadowColor: 'rgba(255,255,255,0.1)',
+                shadowOpacity: 1,
+                shadowOffset: {height:4, width:4},
+                shadowRadius: 2
+            },
+            android: {
+                elevation: 4,
+            }
+        })
+    },
+    imageStyle: {
+       position: 'absolute',
+       top: 0,
+       bottom:0,
+       right: 0,
+       left: 0
+    }
+})
--- src/sections/houses/HousesList.js
+++ src/sections/houses/HousesList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, StyleSheet, Text, FlatList, Button, TouchableOpacity} from 'react-native';
 import { AsyncCalls, Colors } from 'miReact/src/commons'
 import { fetchAlternativo } from 'miReact/src/webservices/webservices'
+import HousesCell from './HousesCell'
 
 export default class HousesList extends Component {
 
@@ -20,48 +21,32 @@ export default class HousesList extends Component {
       }).catch( error => {
           console.log("error: ", error)
       })
-    }
+  }
 
-  checkIsSelected(item){
-    if(this.state.selected && (this.state.selected.id == item.id)){
-      return true
-    } else {
-      return false
-    }
+  onSelect(house) {
+    console.log("house: ", house)
+    this.setState({ selected: house })
   }
 
   renderItem(item, index){
-    const isSelected = this.checkIsSelected(item)
-    const cellStyle = isSelected ? { backgroundColor: 'pink'} : {backgroundColor: 'grey'}
-    const titleStyle = isSelected ? { color: 'grey'} : {color: 'black'}
-    const botonStyle = isSelected ? 'grey' :  'black'
-
     return (
-      <View style={[styles.cell, cellStyle]}>
-        <Text style={titleStyle}>{ item.nombre }</Text>
-        <Text style={titleStyle}>{ item.lema }</Text>
-
-        <TouchableOpacity style={styles.button} 
-          onPress = { () => this.setState({ selected: item }) }>
-          <Text style={styles.buttonText}>{'Seleccionar casa'}</Text>
-        </TouchableOpacity>
-
-      </View>
+      <HousesCell 
+        item = { item }
+        onSelect = { (house) => this.onSelect(house)}  
+      />
     )
   }
 
   render() {
-      const nombre = this.state.selected ? this.state.selected.nombre : ''
-
       return(
         <View style={styles.container}>
 
-          <Text style= {styles.title} > { nombre }</Text>
           <FlatList
             data = { this.state.list }
             renderItem = { ({ item, index }) => this.renderItem(item, index) }
             keyExtractor = {( item, index ) => item.id }
             extraData = { this.state }
+            numColumns = { 2 }
           />
         </View>
       )
@@ -70,29 +55,8 @@ export default class HousesList extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  },
-  cell: {
-    height: 100,
-    marginVertical: 10,
-    padding: 10
-  },
-  title: {
-    fontSize : 20,
-    textAlign: 'center',
-    marginVertical: 20
-  },
-  button: {
-    borderColor: 'white',
-    borderWidth: 1,
-    marginHorizontal: 20,
-    marginVertical: 10,
-    padding: 6,
-    borderRadius: 6
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 18,
-    textAlign: 'center'
+    flex: 1,
+    backgroundColor: 'rgb(42,42,42)',
+    paddingVertical: 30
   }
 });
